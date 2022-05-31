338eece01254981b44dee052c0723b833f17676f
John Rogers <john.g.rogers59.civ@mail.mil>
2022-05-31 18:47:41 -0400

    Fixed bug in the character controller, now you can walk backwards

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -53,7 +53,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			move = transform.InverseTransformDirection(move);
 			CheckGroundStatus();
 			move = Vector3.ProjectOnPlane(move, m_GroundNormal);
-			m_TurnAmount = Mathf.Atan2(move.x, move.z);
+			m_TurnAmount = Mathf.Atan2(move.x, Mathf.Abs(move.z));
 			m_ForwardAmount = move.z;
 
 			ApplyExtraTurnRotation();
